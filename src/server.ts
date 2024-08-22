099c1a1ab198042e270716c7bafb08574c52d757
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-08-22 16:02:49 -0300

    chore: add fastifyCors

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,19 @@
 import fastify from "fastify";
+import fastifyCors from "@fastify/cors";
 
 import { createProduct } from "./routes/create-product";
 import { getProducts } from "./routes/get-products";
-import { getFilteredProducts } from "./routes/get-filtered-products";
 import { deleteProduct } from "./routes/delete-product";
 
 const app = fastify();
 
+app.register(fastifyCors, {
+  origin: "*",
+  methods: ["GET", "POST", "DELETE"],
+});
+
 app.register(createProduct);
 app.register(getProducts);
-app.register(getFilteredProducts);
 app.register(deleteProduct);
 
 app.listen({ port: 3333 }).then(() => console.log("HTTP server running!"));

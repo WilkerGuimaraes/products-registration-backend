19097ec4b9f7e0bd4cc4372be7ec5d646e82c6d2
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-08-22 16:05:05 -0300

    refactor: add filtering and pagination to getProducts endpoint and remove get-filterd-products route

--- src/routes/get-products.ts
+++ src/routes/get-products.ts
@@ -5,28 +5,46 @@ import { prisma } from "../lib/prisma";
 import { ClientError } from "../errors/client-error";
 
 export async function getProducts(app: FastifyInstance) {
-  app.get("/products/:page", async (request, reply) => {
-    const getProcutsParams = z.object({
-      page: z.string().regex(/^\d+$/), // apenas números inteiros
+  app.get("/products", async (request, reply) => {
+    const searchParams = z.object({
+      id: z.string().cuid().optional(),
+      name: z.string().optional(),
+      page: z.string().regex(/^\d+$/).optional(),
     });
 
-    const { page } = getProcutsParams.parse(request.params);
+    const { id, name, page } = searchParams.parse(request.query);
 
-    const currentPage = parseInt(page, 10); // converte para número inteiro
-    const offset = (currentPage - 1) * 20; // calcula o offset
-
+    const currentPage = page ? parseInt(page, 10) : 1;
+    const offset = (currentPage - 1) * 20;
     const products = await prisma.product.findMany({
+      where: {
+        id,
+        name: {
+          contains: name?.toLocaleLowerCase(),
+          mode: "insensitive",
+        },
+      },
       orderBy: {
-        createdAt: "desc",
+        createdAt: "asc",
       },
       take: 20,
       skip: offset,
     });
 
     if (products.length === 0) {
-      return new ClientError("Bad Request in the URL.");
+      return new ClientError("Product not found or not exist.");
     }
 
-    return reply.send({ products });
+    const count = await prisma.product.count({
+      where: {
+        id,
+        name: {
+          contains: name?.toLocaleLowerCase(),
+          mode: "insensitive",
+        },
+      },
+    });
+
+    return reply.send({ products, count });
   });
 }
